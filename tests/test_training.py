7e5b58ff6f456d0bb880b50267fb7c285df1bf7e
AnneKDesler <s194333@dtu.dk>
2023-01-10 14:02:30 +0100

    ignore if no data

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,8 +1,13 @@
 import torch
 from src.models.model import MyAwesomeModel
+from tests import _PATH_DATA
+import pytest
+import os
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA + "/processed/train.pt"), reason="Data files not found")
 def test_training():
 
-    train_set = torch.load("data/processed/train.pt")
+    train_set = torch.load(_PATH_DATA + "/processed/train.pt")
     trainloader = torch.utils.data.DataLoader(train_set, batch_size=10)
 
     batch = next(iter(trainloader))
